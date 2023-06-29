21afa3c7c9e3c46a022e6993a7bb5dc645822c1d
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-06-29 18:17:02 -0400

    tidying up

---
+++ index.js
@@ -0,0 +1,4 @@
+const bloomtechButton = document.querySelector(".bloom-button");
+bloomtechButton.addEventListener("click", () => {
+  alert("This feature is under construction, sorry for the inconvenience!");
+});
